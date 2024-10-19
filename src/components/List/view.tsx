7badfed9cb3641f796abbf34adfadbae4eff3095
ramesyroy292 <rameshroy292@gmail.com>
2024-10-19 20:36:21 +0530

    updated results heading

--- src/components/List/view.tsx
+++ src/components/List/view.tsx
@@ -4,8 +4,9 @@ import {ListComponentProps} from '../../interfaces';
 import {HighlightText, ExternalLink} from '../index';
 
 const List: React.FC<ListComponentProps> = ({ListData}) => {
-    const {ResultItems} = ListData;    
+    const {ResultItems, Page, PageSize, TotalNumberOfResults} = ListData;    
     return <>
+        <h3 className="searchResults">Showing {PageSize*Page-PageSize || 1}-{PageSize*Page} of {TotalNumberOfResults} results</h3>
         {ResultItems?.map((item:any) =>
             <div className="list-container" key={item.DocumentId}> 
                 <HighlightText className="hero-text" type="heading" Title={item.DocumentTitle} />
