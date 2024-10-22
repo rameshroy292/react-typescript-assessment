9dc66a272298904f33614c5ce7a7036daf637401
ramesyroy292 <rameshroy292@gmail.com>
2024-10-22 08:11:29 +0530

    updated e2e test cases by using cypress

--- src/utils/fetchAPI.ts
+++ src/utils/fetchAPI.ts
@@ -1,10 +1,12 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export const apiCall = async(
     endPoint: string,
     method: string,
     data: any = null,
 ) => {
     try{
-        const response = await fetch(`${endPoint}`,{
+        const response = await fetch(`${API_BASE_URL}${endPoint}`,{
             method,
             headers: {
                 'Content-Type': 'text/plain; charset=utf-8',
